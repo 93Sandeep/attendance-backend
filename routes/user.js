0c0ad4053d698cd0cc27473d3a0afd0e82579bf3
93Sandeep <sandeep.kumar@codalien.com>
2024-01-16 11:46:16 +0530

    backend completed

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import userController from '../Controllers/userController.js';
+import {signup} from '../Controllers/authController.js';
+
+const router = express.Router();
+
+// router.get('/all', userController.getAllUsers);
+router.get('/user/:code', userController.getSpecificUser);
+router.get('/punchin', userController. getPunchDataFromBiometricDevice);
+router.get('/getUsers', userController.getAllUsers);
+router.get('/monthlyreport',userController.getMonthlyReport)
+
+
+
+export default router;
+
